85d4202f7bbfbe82fdf45aa1e0b5d3487273a734
James Drucza <james@drucza.net>
2013-03-17 16:49:50 +1100

    app consulting page done and some general tidy up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 NativetongueWebsite::Application.routes.draw do
+  match '/app-consulting' => 'home#app_consulting', :as => 'consulting'
   authenticated :user do
     root :to => 'home#index'
   end
