46c3234b7a759a6cbc15002cf5769a541faff9da
James Drucza <james@drucza.net>
2013-03-25 11:20:24 +1100

    reinstating apps subdomain as this did not fix the issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 NativetongueWebsite::Application.routes.draw do
   match '/app-consulting' => 'home#app_consulting', :as => 'consulting'
-  #match '/' => 'home#app_consulting', :constraints => {:subdomain => 'apps'}
+  match '/' => 'home#app_consulting', :constraints => {:subdomain => 'apps'}
   authenticated :user do
     root :to => 'home#index'
   end
